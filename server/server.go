1730fa3c0cee5071411568a88075be500102ba20
s.vvardenfell <s.vvardenfell@yandex.ru>
2022-07-29 00:38:01 +0300

    refactoring

--- server/server.go
+++ server/server.go
@@ -4,104 +4,159 @@ import (
 	"context"
 	"database/sql"
 
+	_ "github.com/go-sql-driver/mysql"
 	storage "github.com/s-vvardenfell/BooksStorage/books_storage"
 	"github.com/sirupsen/logrus"
 )
 
+const (
+	countBooksQuery = `SELECT COUNT(title) FROM books WHERE author_id = 
+	(SELECT author_id FROM authors WHERE name = ?)`
+	booksQuery = `SELECT title FROM books WHERE author_id = 
+	(SELECT author_id FROM authors WHERE name = ?)`
+	countAuthorsQuery = `SELECT COUNT(name) FROM authors WHERE author_id IN 
+	(SELECT author_id FROM books WHERE title = ?)`
+	authorsQuery = `SELECT name FROM authors WHERE author_id IN 
+	(SELECT author_id FROM books WHERE title = ?)`
+)
+
 type Server struct {
 	storage.UnimplementedBooksStorageServer
-	db *sql.DB
+	cnxn   *sql.DB
+	driver string
+	dsn    string
 }
 
 func New(driver, dsn string) *Server {
-	db, err := sql.Open(driver, dsn)
-	if err != nil {
+	serv := Server{
+		driver: driver,
+		dsn:    dsn,
+	}
+
+	if err := serv.openDbConnection(); err != nil {
 		logrus.Fatalf("cannot open %s connection, %v", driver, err)
 	}
-
-	return &Server{
-		db: db,
-	}
+	return &serv
 }
 
-func (s *Server) GetBooksByAuthors(
-	ctx context.Context, in *storage.Authors) (*storage.Books, error) {
-
-	err := s.db.Ping()
+func (s *Server) openDbConnection() (err error) {
+	s.cnxn, err = sql.Open(s.driver, s.dsn)
 	if err != nil {
-		//TODO RE-OPEN DB (если не откроется снова, слать ошибку)
-		logrus.Errorf("cannot connect with DataBase, %v", err)
-		return nil, err //мб какое-то описание сделать
+		return err
+	}
+	return nil
+}
+
+func (s *Server) GetBooksByAuthor(
+	ctx context.Context, in *storage.Author) (*storage.Books, error) {
+
+	// проверяем соединение с бд
+	if err := s.cnxn.Ping(); err != nil {
+		// если ошибка соединения, пытаемся установить новое
+		if err := s.openDbConnection(); err != nil {
+			logrus.Errorf("cannot re-open %s connection, %v", s.driver, err)
+			return nil, err
+		}
 	}
 
-	stmt, err := s.db.Prepare(
-		`SELECT title FROM books WHERE author_id = 
-		(SELECT author_id FROM authors WHERE name = ?)`)
+	// для того, чтобы выделить достаточно памяти
+	// сразу посчитаем кол-во результатов
+	resultNum, err := s.getResultsCount(countBooksQuery, in.AuthorName)
 	if err != nil {
-		logrus.Errorf("cannot prepare statement, %v", err)
-		return nil, err //мб какое-то описание сделать
+		logrus.Errorf("cannot count results number, %v", err)
+		return nil, err
 	}
-	defer stmt.Close() //TODO лямбда, обработать возврат
 
+	// выделяем память
 	res := &storage.Books{}
-	res.BookName = make([]string, 0)
+	res.BookNames = make([]string, 0, resultNum)
 
-	//TODO здесь будет 1 арг
-	for _, name := range in.AuthorName {
-		row, err := stmt.Query(name)
-		if err != nil {
-			logrus.Errorf("cannot make query, %v", err)
-			return nil, err //мб какое-то описание сделать
-		}
-
-		for row.Next() {
-			var temp string
-			row.Scan(&temp)
-			res.BookName = append(res.BookName, temp)
-		}
+	// делаем запрос в бд
+	if err := s.makeQuery(&res.BookNames, booksQuery, in.AuthorName); err != nil {
+		logrus.Errorf("error during request processing, %v", err)
+		return nil, err
 	}
-
 	return res, nil
 }
 
-func (s *Server) GetAuthorsByBooks(
-	ctx context.Context, in *storage.Books) (*storage.Authors, error) {
+func (s *Server) GetAuthorsByBook(
+	ctx context.Context, in *storage.Book) (*storage.Authors, error) {
 
-	err := s.db.Ping()
-	if err != nil {
-		//TODO RE-OPEN DB (если не откроется снова, слать ошибку)
-		return nil, err //мб какое-то описание сделать
+	// проверяем соединение с бд
+	if err := s.cnxn.Ping(); err != nil {
+		// если ошибка соединения, пытаемся установить новое
+		if err := s.openDbConnection(); err != nil {
+			logrus.Errorf("cannot re-open %s connection, %v", s.driver, err)
+			return nil, err
+		}
 	}
 
-	stmt, err := s.db.Prepare(
-		`SELECT name FROM authors INNER JOIN books 
-		ON authors.author_id = books.author_id 
-		WHERE books.author_id IN 
-		(SELECT author_id FROM books WHERE title = ?)`)
+	// для того, чтобы выделить достаточно памяти
+	// сразу посчитаем кол-во результатов
+	resultNum, err := s.getResultsCount(countAuthorsQuery, in.BookName)
+	if err != nil {
+		logrus.Errorf("cannot count results number, %v", err)
+		return nil, err
+	}
+
+	// выделяем память
+	res := &storage.Authors{}
+	res.AuthorNames = make([]string, 0, resultNum)
+
+	// делаем запрос в бд
+	if err := s.makeQuery(&res.AuthorNames, authorsQuery, in.BookName); err != nil {
+		logrus.Errorf("error during request processing, %v", err)
+		return nil, err
+	}
+	return res, nil
+}
+
+// считает количество результатов для выборки,
+// возвращает число и/или ошибку в случае неудачи
+func (s *Server) getResultsCount(countQuery, prepValue string) (int, error) {
+	var resultNum int
+	// готовим запрос
+	stmt, err := s.cnxn.Prepare(countQuery)
 	if err != nil {
 		logrus.Errorf("cannot prepare statement, %v", err)
-		return nil, err //мб какое-то описание сделать
+		return resultNum, err
 	}
-	defer stmt.Close() //TODO лямбда, обработать возврат
+	defer stmt.Close()
 
-	res := &storage.Authors{}
-	res.AuthorName = make([]string, 0)
+	// получаем результаты
+	err = stmt.QueryRow(prepValue).Scan(&resultNum)
+	if err != nil {
+		logrus.Errorf("cannot make query, %v", err)
+		return resultNum, err
+	}
+	return resultNum, nil
+}
 
-	//TODO здесь будет 1 арг
-	//TODO получить row и потом посчитать кол-во элементов, выделить память
-	for _, name := range in.BookName {
-		row, err := stmt.Query(name)
-		if err != nil {
-			logrus.Errorf("cannot make query, %v", err)
-			return nil, err //мб какое-то описание сделать
-		}
+// делает запрос к базе данных,
+// на вход принимает слайс для хранения результатов,
+// sql-запрос и аргумент для prepared statement,
+// возвращает ошибку в случае неудачи
+func (s *Server) makeQuery(res *[]string, query, prepValue string) error {
+	// готовим запрос
+	stmt, err := s.cnxn.Prepare(query)
+	if err != nil {
+		logrus.Errorf("cannot prepare statement, %v", err)
+		return err
+	}
+	defer stmt.Close()
 
-		for row.Next() {
-			var temp string
-			row.Scan(&temp)
-			res.AuthorName = append(res.AuthorName, temp)
-		}
+	// получаем результаты
+	row, err := stmt.Query(prepValue)
+	if err != nil {
+		logrus.Errorf("cannot make query, %v", err)
+		return err
 	}
 
-	return res, nil
+	// извлекаем
+	for row.Next() {
+		var temp string
+		row.Scan(&temp)
+		*res = append(*res, temp)
+	}
+	return nil
 }
